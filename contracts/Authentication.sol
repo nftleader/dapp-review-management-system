b6f8e4aa4f174ac65eb2dca737ed674de64538a2
captainheart <captainheart@outlook.com>
2018-08-23 10:04:33 +1000

    signup/login done

--- contracts/Authentication.sol
+++ contracts/Authentication.sol
@@ -11,35 +11,162 @@ contract Authentication is Killable {
     rmstoken = RMSToken(rmstokenAddress);
   }
 
+  enum UserType {User, Company}
+  enum ReviewStatus {Pending, Positive, Negative}
+
   struct User {
+    uint id;
+    UserType userType;
+
+    bytes32 email;
     bytes32 user_first_name;
+    bytes32 user_second_name;
+    bytes32 user_zipcode;
+
+    bytes32 company_name;
+    bytes32 company_address;
   }
 
+  struct Product{
+    uint id;
+    uint company_id;
+    bytes32 product_name;
+  }
 
+  struct Review{
+    uint id;
+    uint product_id;
+    uint company_id;
+    uint rating;
+    bool is_spam;
+    bytes32 review;
+    bytes32 reply;
+  }
+
+  //users
   mapping (address => User) private users;
+  mapping (uint => address) private usersById;
+  uint public userCount;
+  uint public totalCount;
+  uint public companyCount;
+
+  //reviews
+  mapping (uint => Review) private reviewById;
+  uint public reviewCount;
+  
 
   uint private id; // Stores user id temporarily
 
-  modifier onlyExistingUser {
-    // Check if user exists or terminate
+  modifier onlyExistingUser() { require(users[msg.sender].id > 0, "User is not registered"); _; }
+  modifier onlyNONExistingUser() { require(users[msg.sender].id == 0, "User is already registered"); _; }
+  modifier onlyExistingUserID(uint userid) { require(usersById[userid] != 0, "User ID is not registered"); _; }
 
-    require(!(users[msg.sender].user_first_name == 0x0));
+  modifier onlyCompany{
+    require(users[msg.sender].id > 0, "User does not exist");
+    require(users[msg.sender].userType == UserType.Company, "User is not company");
     _;
   }
 
-  modifier onlyValidName(bytes32 user_first_name) {
-    // Only valid names allowed
-
-    require(!(user_first_name == 0x0));
+  modifier onlyUser{
+    require(users[msg.sender].id > 0, "User does not exist");
+    require(users[msg.sender].userType == UserType.User, "User is not company");
     _;
   }
 
-  function login() constant
-  public
-  onlyExistingUser
-  returns (bytes32) {
-    return (users[msg.sender].user_first_name);
+  modifier onlyValidName(bytes32 user_first_name) {require(!(user_first_name == 0x0), "invalid name");_;}
+  modifier onlyValidEmail(bytes32 email) { require(!(email == 0x0), "Invalid email"); _; }
+
+  event LogUserSignUp(address from);
+  event LogCompanySignUp(address from);
+
+  event LogBytes32(bytes32 data);
+  event LogString(string);
+  event LogNumber(uint num);
+
+  function login()
+  public /*constant*/ payable  onlyExistingUser
+  returns (uint, UserType, bytes32, bytes32, bytes32, bytes32,  bytes32, bytes32) {
+
+    // emit LogString("-------  Login");
+    // emit LogNumber(users[msg.sender].id);
+    // emit LogBytes32(users[msg.sender].email);
+    // emit LogBytes32(users[msg.sender].user_first_name);
+    // emit LogBytes32(users[msg.sender].user_second_name);
+    // emit LogBytes32(users[msg.sender].user_zipcode);
+    // emit LogBytes32(users[msg.sender].company_name);
+    // emit LogBytes32(users[msg.sender].company_address);
+    return (users[msg.sender].id, 
+            users[msg.sender].userType,
+            users[msg.sender].email,
+            users[msg.sender].user_first_name, 
+            users[msg.sender].user_second_name, 
+            users[msg.sender].user_zipcode, 
+            users[msg.sender].company_name, 
+            users[msg.sender].company_address);
   }
+
+  /** @dev convert strint to bytes32
+     */
+  function stringToBytes32(string memory source) internal pure returns (bytes32 result) {
+      bytes memory tempEmptyStringTest = bytes(source);
+      if (tempEmptyStringTest.length == 0) {
+          return 0x0;
+      }
+
+      assembly {
+          result := mload(add(source, 32))
+      }
+  }
+
+  function signupUser(
+    string _email,
+    string _user_first_name,
+    string _user_second_name,
+    string _user_zipcode
+  )
+  onlyNONExistingUser
+  external  payable  returns (uint) {
+    User memory newbie;
+    newbie.email              = stringToBytes32(_email);
+    newbie.user_first_name    = stringToBytes32(_user_first_name);
+    newbie.user_second_name   = stringToBytes32(_user_second_name);
+    newbie.user_zipcode       = stringToBytes32(_user_zipcode);
+    userCount++;
+    totalCount++;
+    newbie.id = totalCount;
+    newbie.userType = UserType.User;
+
+    usersById[totalCount] = msg.sender;
+    users[msg.sender] = newbie;
+    emit LogUserSignUp(msg.sender);
+    emit LogNumber(totalCount);
+    return totalCount; //return userid
+  }
+
+
+  function signupCompany(
+    string _email,
+    string _company_name,
+    string _company_address
+  )
+  onlyNONExistingUser
+  external  payable  returns (uint) {
+    User memory newbie;
+    newbie.email              = stringToBytes32(_email);
+    newbie.company_name    = stringToBytes32(_company_name);
+    newbie.company_address       = stringToBytes32(_company_address);
+    companyCount++;
+    totalCount++;
+    newbie.id = totalCount;
+    newbie.userType = UserType.Company;
+
+    usersById[totalCount] = msg.sender;
+    users[msg.sender] = newbie;
+    emit LogUserSignUp(msg.sender);
+    emit LogNumber(totalCount);
+    return totalCount; //return userid
+  }
+
 
   function signup(bytes32 user_first_name)
   public
--- test/authentication.js
+++ test/authentication.js
@@ -15,9 +15,55 @@ contract('Authentication', async function(accounts) {
   });
   
   it("...should sign up and log in a user.", async function() {
-    await authentication.signup('testuser', {from: accounts[0]});
-    let userName = await  authentication.login.call();
-    assert.equal(web3.toUtf8(userName), 'testuser', "The user was not signed up.");
+    let expectedEmail = 'first@user.com';
+    let expectedFirstName = 'first name';
+    let expectedSecondName = 'second name';
+    let expectedZipCode = '65432432';
+
+    await authentication.signupUser(expectedEmail, expectedFirstName, expectedSecondName, expectedZipCode, {from: accounts[0]});
+    let [id, userType, email, user_first_name, user_second_name, user_zipcode, company_name, company_address] = await authentication.login.call({from: accounts[0]});
+      
+    assert.equal(userType.toNumber(), 0, "The user is company.");
+    assert.equal(web3.toUtf8(email), expectedEmail, "email is wrong.");
+    assert.equal(web3.toUtf8(user_zipcode), expectedZipCode, "zipcode is wrong.");
   });
 
+  
+  it("...should sign up and log in a company.", async function() {
+    let expectedEmail = 'company@company.com';
+    let expectedName = 'my great company';
+    let expectedAddress = 'vladivostok russia';
+
+    await authentication.signupCompany(expectedEmail, expectedName, expectedAddress, {from: accounts[0]});
+    let [id, userType, email, user_first_name, user_second_name, user_zipcode, company_name, company_address] = await authentication.login.call({from: accounts[0]});
+
+    assert.equal(userType.toNumber(), 1, "The user is company.");
+    assert.equal(web3.toUtf8(email), expectedEmail, "email is wrong.");
+    assert.equal(web3.toUtf8(company_address), expectedAddress, "company address is wrong.");
+  });
+
+
+  it("...should sign up 3 users and 2 company and list them all.", async function() {
+    //user
+    await authentication.signupUser('first@user.com', 'first name 1', 'second name 1', '65432432', {from: accounts[0]});
+    await authentication.signupUser('second@user.com', 'first name 2', 'second name 2 ', '34546532', {from: accounts[1]});
+    await authentication.signupUser('third@user.com', 'first name 3', 'second name 3 ', '6875687354', {from: accounts[2]});
+    //company
+    await authentication.signupCompany('company@company.com', 'my great company', 'vladivostok russia', {from: accounts[3]});
+    await authentication.signupCompany('coporation@coporation.com', 'great coporation', 'russia vladivostok', {from: accounts[4]});
+
+    let [id, userType, email, user_first_name, user_second_name, user_zipcode, company_name, company_address] = await authentication.login.call({from: accounts[3]});
+            console.log("**** id: ", id.toNumber());
+            console.log("**** userType: ", userType.toNumber());
+            console.log("**** email: ", web3.toUtf8(email));
+            console.log("**** user_first_name: ", web3.toUtf8(user_first_name));
+            console.log("**** user_second_name: ", web3.toUtf8(user_second_name));
+            console.log("**** user_zipcode: ", web3.toUtf8(user_zipcode));
+            console.log("**** company_name: ", web3.toUtf8(company_name));
+            console.log("**** company_address: ", web3.toUtf8(company_address));
+
+    assert.equal(userType.toNumber(), 1, "The user is User.");
+    assert.equal(web3.toUtf8(email), 'company@company.com', "email is wrong.");
+    assert.equal(web3.toUtf8(company_address), 'vladivostok russia', "company address is wrong.");
+  });
 });
